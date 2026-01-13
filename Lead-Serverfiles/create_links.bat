baab1fe6df49f7d5c1b8c8228c67e5c4495be56b
Lead0x194 <not_found@lead.com>
2026-01-13 08:05:22 +0100

    chore: add batch script to create links

---
+++ Lead-Serverfiles/create_links.bat
@@ -0,0 +1,12 @@
+@echo off
+
+for %%x in (db, auth, channel1/game1, channel1/game2, game99) do (
+
+	@rmdir /q "C:/Lead-Git/Lead-Serverfiles/%%x/data" >nul 2>&1
+	@del /Q "C:/Lead-Git/Lead-Serverfiles/%%x\data" >nul 2>&1
+	@mklink /D "C:/Lead-Git/Lead-Serverfiles/%%x/data" "C:/Lead-Git/Lead-Serverfiles/share/data"
+
+	rmdir /q "C:/Lead-Git/Lead-Serverfiles/%%x/locale" >nul 2>&1
+	@del /Q "C:/Lead-Git/Lead-Serverfiles/%%x\locale" >nul 2>&1
+	@mklink /D "C:/Lead-Git/Lead-Serverfiles/%%x/locale" "C:/Lead-Git/Lead-Serverfiles/share/locale"
+)
