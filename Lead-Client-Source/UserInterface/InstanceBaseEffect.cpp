8c0b8bab96e32bdd0a34bce94ec9b14e9969c9cd
Dex <root@localhost>
2026-01-25 22:36:59 +0100

    Fixing realtime level refresh of players.
    https://metin2.dev/topic/30612-official-level-update-fix-reversed/

--- Lead-Client-Source/UserInterface/InstanceBaseEffect.cpp
+++ Lead-Client-Source/UserInterface/InstanceBaseEffect.cpp
@@ -656,6 +656,8 @@ void CInstanceBase::UpdateTextTailLevel(DWORD level)
 	//static D3DXCOLOR s_kLevelColor = D3DXCOLOR(119.0f/255.0f, 246.0f/255.0f, 168.0f/255.0f, 1.0f);
 	static D3DXCOLOR s_kLevelColor = D3DXCOLOR(152.0f/255.0f, 255.0f/255.0f, 51.0f/255.0f, 1.0f);
 
+	m_dwLevel = level;
+
 	char szText[256];
 	sprintf(szText, "Lv. %d ", level);
 	CPythonTextTail::Instance().AttachLevel(GetVirtualID(), szText, s_kLevelColor);
